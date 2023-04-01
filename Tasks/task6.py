6c6bb26fb4248c0ee2d557200dbbcfb5b9e16c02
nirmohi2708 <nirmohithakkar@gmail.com>
2023-04-01 15:29:15 +0530

    merge other

---
+++ Tasks/task6.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the value of a :"))
+b=int(input("Enter the value of b :"))
+c=int(input("Enter the value of c :"))
+
+if(a>b):
+
+    if(a>c):	
+        print("a is maximum")
+    
+    else:
+        print("c is maximum")
+    
+else :
+    print("b is maximum")
