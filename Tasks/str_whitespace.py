b5ea097cd6b08eb833dc5a2d6c51a8e7831afa5d
nirmohi2708 <nirmohithakkar@gmail.com>
2023-04-15 14:39:43 +0530

    merge other

--- Tasks/str_whitespace.py
+++ Tasks/str_whitespace.py
@@ -1,2 +1,13 @@
+# WAP to identify the white spaces in the string entered by the user
+
 v=input("Enter string :")
+
+count=0
+
+for i in range(0,len(v)):
+    if v[i]==" ":
+        count+=1
+
+print("White spaces : ",count)
+    
 
